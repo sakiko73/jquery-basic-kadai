9fbf8690a64c6ee0ed40c4d5bfe449501adac0a2
sakiko73 <159975716+sakiko73@users.noreply.github.com>
2024-04-17 21:03:48 +0900

    課題を提出いたします

---
+++ kadai_004/event.js
@@ -0,0 +1,12 @@
+ $(window).on('load', function(){
+    //「loadイベントが発生しました」
+    console.log('loadイベントが発生しました');
+    });
+
+ $(window).on('scroll', function(){
+    //「scrollイベントが発生しました」
+    console.log('scrollイベントが発生しました');
+    });
+
+
+//参考コード　　$(window).on(イベント, () => {});
